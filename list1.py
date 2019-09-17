f147e389189f9abd61deb1d67922b47d448479dc
Andrew Christensen <andrewpchristensen701@gmail.com>
2019-09-17 12:49:25 -0400

    true final commit

--- list1.py
+++ list1.py
@@ -25,8 +25,12 @@
 
 
 def match_ends(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    stringlength = 0
+
+    for word in words:
+         if len(word) > 1 and word[0] == word[-1]:
+              stringlength +=1
+    return stringlength
 
 
 # B. front_x
@@ -37,8 +41,14 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    xlist=[]
+    def sort(y):
+         for elem in y[:]:
+              if elem.startswith('x'):
+                   xlist.append(elem)
+                   y.remove(elem)
+
+    return xlist
 
 
 # C. sort_last
@@ -47,9 +57,10 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+def last(n): return n[-1]
 def sort_last(tuples):
-    """Your code goes here.  Edit this docstring."""
-    return
+    sortedlast = sorted(tuples,key=last)
+    return sortedlast
 
 
 # Simple provided test() function used in main() to print
